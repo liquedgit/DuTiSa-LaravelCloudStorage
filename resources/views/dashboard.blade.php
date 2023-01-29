8aff61d97b1017b5c48a37ad7bb2b8d68a046274
Dep <daveandrewnathaniel48@gmail.com>
2023-01-29 18:29:44 +0700

    merge T27 -> T49

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,9 +13,6 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"
         integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body class="container">
